74185fa30235e080e1648d3babe7037cd7b838c1
Sebastian Hebel <sebah1989@gmail.com>
2014-05-16 19:27:10 +0200

    tests refactor

--- allegro/src/source/Main.java
+++ allegro/src/source/Main.java
@@ -1,9 +1,17 @@
 package source;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class Main {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		Manager manager = new Manager();
-		manager.start();
+		//manager.start();
+		Document doc = Jsoup.connect("http://allegro.pl/dzial/strefa-okazji/").get();
+		String cos = doc.select("section.category.promotion-category.separator-bottom.transform").first().toString();
+		System.out.println(cos);
 	}
 
 } 
--- allegro/src/tests/AllegroTests.java
+++ allegro/src/tests/AllegroTests.java
@@ -6,73 +6,30 @@ import org.jsoup.nodes.Element;
 import org.junit.Assert;
 import org.junit.Test;
 
-import source.Allegro;
+import source.*;
+import source.Item;
+import source.Manager;
 
 public class AllegroTests {
 	
 	@Test
-	public void testgetDataFromAllegroWebsite() {
-		Assert.assertNotNull("response from website shouldn't be null", Allegro.getDataFromAllegroWebsite());
+	public void testPlainItem() {
+		Item item = new PlainItem(elementForPlainItem);
+		Assert.assertEquals("item should have proper prices difference", 400.0, item.getDifferenceBetweenPrices(),0.01);
 	}
 	
 	@Test
-	public void testGetCategoriesFromDocument(){
-		String html = "<html><head><title>Some title</title></head><body><div class='category' data-category-main='false' data-category-id='100008'>"
-				+ "<h3>Nowe oferty</h3></div><div class='category' data-category-main='false' data-category-id='100008'> + "
-				+ "<h3>Nowe oferty</h3></div></body></html>";
-		Document doc = Jsoup.parse(html);
-		Assert.assertEquals("method should return proper number of categories", 2, Allegro.getCategoriesFromDocument(doc).size());
+	public void testPromoItem() {
+		PromoItem promoItem = new PromoItem(elementForPromoItem);
+		Assert.assertEquals("item should have proper prices difference", 300.0, promoItem.getDifferenceBetweenPrices(),0.01);
 	}
 	
-	@Test
-	public void testGetCategoryName(){
-		String html = "<html><head><title>Some title</title></head><body><div class='category' data-category-main='false' data-category-id='100008'>"
-				+ "<h3>Nowe oferty</h3></div></body></html>";
-		Document doc = Jsoup.parse(html);
-		Element element = Allegro.getCategoriesFromDocument(doc).first();
-		Assert.assertEquals("method should return proper category name", "Nowe oferty", Allegro.getCategoryName(element));
-	}
-	
-	@Test
-	public void testGetItemsFromCategory(){
-		String html = "<html><head><title>Some title</title></head><body><div class='category' data-category-main='false' data-category-id='100008'>"
-				+ "<h3>Nowe oferty</h3><li class=''></li><li class=''></li><li class=''></li></div><div class='category' data-category-main='false' data-category-id='100008'> + "
-				+ "<h3>Nowe oferty</h3><li class=''></li><li class=''></li><li class=''></li></div></body></html>";
-		Document doc = Jsoup.parse(html);
-		Element element = Allegro.getCategoriesFromDocument(doc).first();
-		Assert.assertEquals("method should return proper number of li elements", 3, Allegro.getItemsFromCategory(element).size());
-	}
-	@Test
-	public void testParsingDocumentBeforeReductionpriceToString(){
-		String html = "<html><head><title>Some title</title></head><body><div class='category' data-category-main='false' data-category-id='100008'>"
-				+ "<h3>Nowe oferty</h3><li class=''><div class='price-container clearfix'><span class='price bargains-main-color'> +"
-				+ "1 499,00 zł</span><del>1 899,00 zł</del></div></li><li class=''></li><li class=''></li></div></body></html>";
-		Document doc = Jsoup.parse(html);
-		Element element = Allegro.getCategoriesFromDocument(doc).first();
-		Assert.assertEquals("method should return proper before reduction price", "1 899,00 zł",Allegro.parseDocumentBeforeReductionPriceToString(element));
-	}
-	
-	@Test
-	public void testParsingDocumentAfterReductionpriceToString(){
-		String html = "<html><head><title>Some title</title></head><body><div class='category' data-category-main='false' data-category-id='100008'> + "
+	private String htmlForPlainItem = "<div class='category' data-category-main='false' data-category-id='100008'> " 
 				+ "<h3>Nowe oferty</h3><li class=''><div class='price-container clearfix'><span class='price bargains-main-color'>1 499,00 zł</span>"
-				+ "<del>1 899,00 zł</del></div></li><li class=''></li><li class=''></li></div></body></html>";
-		Document doc = Jsoup.parse(html);
-		Element element = Allegro.getCategoriesFromDocument(doc).first();
-		Assert.assertEquals("method should return proper after reduction price", "1 499,00 zł", Allegro.parseDocumentAfterReductionPriceToString(element));
-	}
-	@Test
-	public void testParsingStringpriceToDouble(){
-		String price = "1 499,00 zł";
-		Assert.assertEquals("method should parse String price to double", 1499.00, Allegro.parseStringPriceToDouble(price), 0.1);
-		
-	}
-	@Test
-	public void testCheckingDifferenceBeewtenTwoprices(){
-		double priceBefore = 1899.0;
-		double priceAfter = 1499.0;
-		Assert.assertEquals("method should return proper difference", 400.00, Allegro.getDifferenceBetweenPrices(priceBefore, priceAfter), 0.1);
-		
-	}
-
+				+ "<del>1 899,00 zł</del></div></li><li class=''></li><li class=''></li></div>";
+	private Element elementForPlainItem = Jsoup.parse(htmlForPlainItem);
+	private String htmlForPromoItem = "&quot;installmentAvailable&quot;:true,&quot;regularPriceFormatted&quot;:&quot;1\\u00a0799,00\\u00a0z\\u0142&quot;,&quot;specialPriceFormatted&quot;:&quot;1\\u00a0499,00"
+			+ "\\u00a0z\\u0142&quot;,&quot;priceWithShippingFormatted&quot;:&quot";
+	private Element elementForPromoItem = Jsoup.parse(htmlForPromoItem);
+	private String htmlForPromoCategory = "<li data-bargain-info=\"[{&quot><li>";
 }
